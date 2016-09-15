c1cfe87064cf4736ee0ef00ce1fd98c983f94592
qtrading <qtrading@QTRADING>
2016-09-15 03:52:04 +0000

    add button and press event binding

--- FSharpFormApp101/FSharpFormApp101/Program.fs
+++ FSharpFormApp101/FSharpFormApp101/Program.fs
@@ -6,7 +6,10 @@ open System.Windows.Forms
 
 [<EntryPoint>]
 let main argv = 
-    use form = Form()
+    let form = new Form(Text = "First F# form")
+    let button = new Button(Text = "Click me to close!", Dock = DockStyle.Fill)
+    button.Click.Add(fun _ -> Application.Exit() |> ignore)
+    form.Controls.Add(button)
     Application.Run(form);
     0
        
